db839ab2f2bba121c4d90b7c9da33da2a3518be3
Manoj Saini <manojsaini513@gmail.com>
2023-07-01 15:24:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
-
+	const result = document.getElementById("form1");
+	const fname = result.elements['fname'].value;
+	const lname = result.elements['lname'].value;
+	alert(`${fname} ${lname}`);
 }
